f44debeebd09e47aaf7e9bb63e12813c33d27730
User1 <User1@adm-48594>
2016-02-29 14:42:08 +0100

    wery

--- AlarmManagement/src/gui/FXMLDocumentController.java
+++ AlarmManagement/src/gui/FXMLDocumentController.java
@@ -9,6 +9,6 @@ public class FXMLDocumentController implements Initializable
     @Override
     public void initialize(URL url, ResourceBundle rb)
     {
-        // TODO
+        // TODO ufitfyikfiuftyu
     }    
 }
