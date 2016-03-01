d4c7616d4ccdb026b4844e7393dc743c553a89df
Pogoe <pogoe_000@Pogoe>
2016-03-01 10:39:16 +0100

    AM1/2

--- AlarmManagement/src/storage/CRUD.java
+++ AlarmManagement/src/storage/CRUD.java
@@ -3,9 +3,13 @@ package storage;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import plc.Solution;
+import plc.Error;
 
 public class CRUD
 {
@@ -23,9 +27,9 @@ public class CRUD
         try
         {
             Class.forName("org.postgresql.Driver");
-            String url = "jdbc:postgresql://localhost:5432/RecipeDB";
-            String user = "postgres"; //TODO: Change to the right value.
-            String pass = "u7e98d22"; //TODO: Change to the right value.
+            String url = "jdbc:postgresql://localhost:5432/RecipeDB"; //TODO: Set up the database and change name
+            String user = "postgres";
+            String pass = "u7e98d22";
             conn = DriverManager.getConnection(url, user, pass);
             errorSt = conn.prepareStatement("SELECT * FROM Error WHERE errorCode = ?;");
             solutionSt = conn.prepareStatement("SELECT * FROM Solution WHERE errorId = ?;");
@@ -37,14 +41,40 @@ public class CRUD
     
     protected List<Solution> getSolutions(String errorCode)
     {
+        List<Solution> l = new ArrayList<>();
         try
         {
             solutionSt.setString(1, errorCode);
+            ResultSet rs = solutionSt.executeQuery();
+            while(rs.next())
+            {
+                l.add(new Solution(rs.getInt("id"), rs.getInt("errorId"), rs.getString("description")));
+            }
         } catch (SQLException ex)
         {
             System.err.println(ex);
         }
         
-        return null;
+        return l;
+    }
+    
+    protected List<Error> getErrors(String errorCode)
+    {
+        List<Error> l = new ArrayList<>();
+        
+        try
+        {
+            errorSt.setString(1, errorCode);
+            ResultSet rs = errorSt.executeQuery();
+            while(rs.next())
+            {
+                l.add(new Error(rs.getInt("id"), rs.getInt("errorCode"), new Date(rs.getTimestamp("time").getTime()), rs.getString("description")));
+            }
+        } catch(SQLException ex)
+        {
+            System.err.println(ex);
+        }
+        
+        return l;
     }
 }
